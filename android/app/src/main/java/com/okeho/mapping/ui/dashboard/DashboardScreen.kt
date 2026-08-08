9963f45e6284769d64c4fa816a896fd7c5ac2b04
Detoye <detoye@cybersphere.africa>
2026-08-08 10:25:54 +0100

    Fix: Add missing Icons.List import

--- android/app/src/main/java/com/okeho/mapping/ui/dashboard/DashboardScreen.kt
+++ android/app/src/main/java/com/okeho/mapping/ui/dashboard/DashboardScreen.kt
@@ -14,6 +14,7 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.List
 import androidx.compose.material.icons.filled.Menu
 import androidx.compose.material.icons.filled.MyLocation
 import androidx.compose.material.icons.filled.Route
