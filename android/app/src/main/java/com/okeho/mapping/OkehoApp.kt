36ae236ae2f1b16c1303b2f3d332b8a4e37f6939
Detoye <detoye@cybersphere.africa>
2026-08-10 16:41:32 +0100

    Fix: Use override val property for workManagerConfiguration

--- android/app/src/main/java/com/okeho/mapping/OkehoApp.kt
+++ android/app/src/main/java/com/okeho/mapping/OkehoApp.kt
@@ -13,11 +13,10 @@ class OkehoApp : Application(), Configuration.Provider {
     @Inject
     lateinit var workerFactory: HiltWorkerFactory
 
-    override fun getWorkManagerConfiguration(): Configuration {
-        return Configuration.Builder()
+    override val workManagerConfiguration: Configuration
+        get() = Configuration.Builder()
             .setWorkerFactory(workerFactory)
             .build()
-    }
 
     override fun onCreate() {
         super.onCreate()
